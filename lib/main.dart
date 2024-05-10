6b5a45aa0d226761e11105a68a2c2ffa46c4f70e
KhaledHAnter <khaldntr11@gmail.com>
2024-05-10 07:04:15 +0300

    gradle

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,24 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tecnosoft_task/Views/cart_view.dart';
+import 'package:tecnosoft_task/Views/forget_password_view.dart';
+import 'package:tecnosoft_task/Views/home_view.dart';
+import 'package:tecnosoft_task/Views/item_details_view.dart';
+import 'package:tecnosoft_task/Views/login_view.dart';
+import 'package:tecnosoft_task/Views/nav_home.dart';
+import 'package:tecnosoft_task/Views/onboarding_view.dart';
+import 'package:tecnosoft_task/Views/register_view.dart';
 import 'package:tecnosoft_task/firebase_options.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  
+
   runApp(const Tecnosoft());
   await ScreenUtil.ensureScreenSize();
-
 }
 
 class Tecnosoft extends StatelessWidget {
@@ -30,8 +37,17 @@ class Tecnosoft extends StatelessWidget {
       },
       debugShowCheckedModeBanner: false,
       title: 'TecnoSoft',
-      
-      
+      routes: {
+        HomeView.id: (context) => const HomeView(),
+        NavHome.id: (context) => const NavHome(),
+        ItemDetails.id: (context) => const ItemDetails(),
+        CartView.id: (context) => const CartView(),
+        OnboardindView.id: (context) => const OnboardindView(),
+        RegisterView.id: (context) => const RegisterView(),
+        LoginView.id: (context) => const LoginView(),
+        ForgetPasswordView.id: (context) => const ForgetPasswordView(),
+      },
+      initialRoute: NavHome.id,
     );
   }
 }
